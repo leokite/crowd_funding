1fd24831def238c2b89edb84dab61fd265f2a7e1
leokite <yuji.kazama.dev@gmail.com>
2018-11-11 10:16:42 +0900

    Add test

--- test/crowd_funding.js
+++ test/crowd_funding.js
@@ -1,7 +1,19 @@
+var CrowdFunding = artifacts.require("CrowdFunding");
+
 contract('CrowdFunding', function(accounts) {
-  it("should assert true", function(done) {
-    var crowd_funding = CrowdFunding.deployed();
-    assert.isTrue(true);
-    done();
+  let instance;
+
+  beforeEach(async () => {
+    instance = await CrowdFunding.deployed();
+  });
+
+  it('should set initial goalAmount as 100000', async () => {
+    let goalAmount = await instance.goalAmount();
+    assert.equal(goalAmount, 100000);
+  });
+
+  it('should not end a campaign in the initial state', async () => {
+    let ended = await instance.ended();
+    assert.equal(ended, false);
   });
 });
