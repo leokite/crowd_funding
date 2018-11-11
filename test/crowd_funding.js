d0555731bd5cc83a7906900b08319f2690cc54b7
leokite <yuji.kazama.dev@gmail.com>
2018-11-11 20:44:53 +0900

    Test

--- test/crowd_funding.js
+++ test/crowd_funding.js
@@ -1,4 +1,5 @@
 import { increaseTime, duration } from './helpers/increaseTime';
+import { getTransactionGasCost } from './helpers/getGasCost';
 
 const BigNumber = web3.BigNumber
 
@@ -10,35 +11,54 @@ require('chai')
 var CrowdFunding = artifacts.require("CrowdFunding");
 
 contract('CrowdFunding', ([owner, investor1, investor2]) => {
+  const DURATION = 30 * 60; // 30 munites
+  const GOAL_AMOUNT = new web3.BigNumber(web3.toWei(1, 'ether'));
+
   let instance;
 
   beforeEach(async () => {
-    let duration = 300;
-    let goalAmount = 100000;
-    instance = await CrowdFunding.new(duration, goalAmount, { from: owner })
-  });
-  6
-
-  it('should set goalAmount', async () => {
-    (await instance.goalAmount()).should.bignumber.equal(100000);
+    instance = await CrowdFunding.new(
+        DURATION, GOAL_AMOUNT, { from: owner })
   });
 
-  it('should not end a campaign in the initial state', async () => {
+  it('should be Funding state in the initial state', async () => {
+    (await instance.status()).should.equal('Funding');
     (await instance.ended()).should.be.false;
   });
 
   it('should fund from investor', async () => {
-    let amount1 = new web3.BigNumber(web3.toWei(0.007, 'ether'));
-    let amount2 = new web3.BigNumber(web3.toWei(0.003, 'ether'));
-    let totalAmount = new web3.BigNumber(web3.toWei(0.01, 'ether'));
-    instance.fund({ from: investor1, value: amount1 });
-    instance.fund({ from: investor1, value: amount2 });
+    let amount1 = new web3.BigNumber(web3.toWei(0.1, 'ether'));
+    let amount2 = new web3.BigNumber(web3.toWei(0.1, 'ether'));
+    await instance.fund({ from: investor1, value: amount1 });
+    await instance.fund({ from: investor2, value: amount2 });
     (await instance.investors(0))[1].should.bignumber.equal(amount1);
     (await instance.investors(1))[1].should.bignumber.equal(amount2);
-    (await instance.totalAmount()).should.bignumber.equal(totalAmount);
-    (await web3.eth.getBalance(instance.address)).should.bignumber.equal(totalAmount);
+    (await instance.totalAmount()).should.bignumber.equal(amount1.plus(amount2));
+    (await web3.eth.getBalance(instance.address)).should.bignumber.equal(amount1.plus(amount2));
   });
 
+  it('should success the campaign if totalAmount is reached by deadline', async () => {
+    let amount1 = new web3.BigNumber(web3.toWei(1, 'ether'));
+    await instance.fund({ from: investor1, value: amount1 });
+    await increaseTime(duration.hours(1));
+    await instance.checkGoalReached({ from: owner });
+    (await instance.status()).should.be.equal("Campaign Succeeded");
+    (await instance.ended()).should.be.true;
+    (await web3.eth.getBalance(instance.address)).should.bignumber.equal(0);
+  });
+
+  it('should fail the campaign if totalAmount is NOT reached by deadline', async () => {
+    const amount1 = new web3.BigNumber(web3.toWei(0.1, 'ether'));
+    const pre = web3.eth.getBalance(investor1);
+    const res = await instance.fund({ from: investor1, value: amount1 });
+    const gasCost = getTransactionGasCost(res['tx']);
+    await increaseTime(duration.hours(1));
+    await instance.checkGoalReached({ from: owner });
+    const post = web3.eth.getBalance(investor1);
+    (await instance.status()).should.be.equal("Campaign Failed");
+    (await instance.ended()).should.be.true;
+    post.minus(pre).plus(gasCost).should.be.bignumber.equal(0);
+  });
 
 });
-6
+
--- test/helpers/increaseTime.js
+++ test/helpers/increaseTime.js
@@ -1,3 +1,12 @@
+export const duration = {
+  seconds: function (val) { return val; },
+  minutes: function (val) { return val * this.seconds(60); },
+  hours: function (val) { return val * this.minutes(60); },
+  days: function (val) { return val * this.hours(24); },
+  weeks: function (val) { return val * this.days(7); },
+  years: function (val) { return val * this.days(365); },
+};
+
 export function increaseTime(duration) {
   return new Promise((resolve, reject) => {
     web3.currentProvider.sendAsync({
@@ -12,11 +21,3 @@ export function increaseTime(duration) {
   })
 }
 
-export const duration = {
-  seconds: function (val) { return val; },
-  minutes: function (val) { return val * this.seconds(60); },
-  hours: function (val) { return val * this.minutes(60); },
-  days: function (val) { return val * this.hours(24); },
-  weeks: function (val) { return val * this.days(7); },
-  years: function (val) { return val * this.days(365); },
-};
