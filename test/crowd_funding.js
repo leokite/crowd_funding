3d973eb15674b30a0e1154cb2538f6406a066e35
leokite <yuji.kazama.dev@gmail.com>
2018-11-11 14:57:21 +0900

    Misc

--- test/crowd_funding.js
+++ test/crowd_funding.js
@@ -1,19 +1,44 @@
+import { increaseTime, duration } from './helpers/increaseTime';
+
+const BigNumber = web3.BigNumber
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
+
 var CrowdFunding = artifacts.require("CrowdFunding");
 
-contract('CrowdFunding', function(accounts) {
+contract('CrowdFunding', ([owner, investor1, investor2]) => {
   let instance;
 
   beforeEach(async () => {
-    instance = await CrowdFunding.deployed();
+    let duration = 300;
+    let goalAmount = 100000;
+    instance = await CrowdFunding.new(duration, goalAmount, { from: owner })
   });
+  6
 
-  it('should set initial goalAmount as 100000', async () => {
-    let goalAmount = await instance.goalAmount();
-    assert.equal(goalAmount, 100000);
+  it('should set goalAmount', async () => {
+    (await instance.goalAmount()).should.bignumber.equal(100000);
   });
 
   it('should not end a campaign in the initial state', async () => {
-    let ended = await instance.ended();
-    assert.equal(ended, false);
+    (await instance.ended()).should.be.false;
   });
+
+  it('should fund from investor', async () => {
+    let amount1 = new web3.BigNumber(web3.toWei(0.007, 'ether'));
+    let amount2 = new web3.BigNumber(web3.toWei(0.003, 'ether'));
+    let totalAmount = new web3.BigNumber(web3.toWei(0.01, 'ether'));
+    instance.fund({ from: investor1, value: amount1 });
+    instance.fund({ from: investor1, value: amount2 });
+    (await instance.investors(0))[1].should.bignumber.equal(amount1);
+    (await instance.investors(1))[1].should.bignumber.equal(amount2);
+    (await instance.totalAmount()).should.bignumber.equal(totalAmount);
+    (await web3.eth.getBalance(instance.address)).should.bignumber.equal(totalAmount);
+  });
+
+
 });
+6
