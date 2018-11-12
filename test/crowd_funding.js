de1e7925134196819a8d920f0f2b95a8d444474f
fx27193 <Kazama.Yuji@fujixerox.co.jp>
2018-11-12 10:55:45 +0900

    Refactor

--- test/crowd_funding.js
+++ test/crowd_funding.js
@@ -11,42 +11,41 @@ require('chai')
 var CrowdFunding = artifacts.require("CrowdFunding");
 
 contract('CrowdFunding', ([owner, investor1, investor2]) => {
-  const DURATION = 30 * 60; // 30 munites
+  const DURATION = 1800; // 30 munites
   const GOAL_AMOUNT = new web3.BigNumber(web3.toWei(1, 'ether'));
 
   let instance;
 
   beforeEach(async () => {
-    instance = await CrowdFunding.new(
-        DURATION, GOAL_AMOUNT, { from: owner })
+    instance = await CrowdFunding.new(DURATION, GOAL_AMOUNT, { from: owner });
   });
 
-  it('should be Funding state and not end the campaign initially', async () => {
+  it('should be Funding state initially', async () => {
     (await instance.status()).should.equal('Funding');
     (await instance.ended()).should.be.false;
   });
 
-  it('should fail checkGoalReaced is called before campaign end', async() => {
+  it('should fail when checkGoalReaced is called before campaign end', async() => {
     instance.checkGoalReached({ from: owner }).should.be.rejectedWith('revert');
   });
 
-  it('should fail checkGoalReaced is called by other than owner', async() => {
-    let amount1 = new web3.BigNumber(web3.toWei(0.1, 'ether'));
+  it('should fail when checkGoalReaced is called by other than owner', async() => {
+    const amount1 = new web3.BigNumber(web3.toWei(0.1, 'ether'));
     await instance.fund({ from: investor1, value: amount1 });
     instance.checkGoalReached({ from: investor1 }).should.be.rejectedWith('revert');
   });
 
-  it('should fail to fund after the campaign end', async () => {
-    let amount1 = new web3.BigNumber(web3.toWei(1, 'ether'));
+  it('should fail when fund is called after the campaign end', async () => {
+    const amount1 = new web3.BigNumber(web3.toWei(1, 'ether'));
     await instance.fund({ from: investor1, value: amount1 });
     await increaseTime(duration.hours(1));
     await instance.checkGoalReached({ from: owner });
     instance.fund({ from: investor1, value: amount1 }).should.be.rejectedWith('revert');
   });
 
-  it('should fund is called by investors', async () => {
-    let amount1 = new web3.BigNumber(web3.toWei(0.1, 'ether'));
-    let amount2 = new web3.BigNumber(web3.toWei(0.1, 'ether'));
+  it('should fund from investors', async () => {
+    const amount1 = new web3.BigNumber(web3.toWei(0.1, 'ether'));
+    const amount2 = new web3.BigNumber(web3.toWei(0.1, 'ether'));
     await instance.fund({ from: investor1, value: amount1 });
     await instance.fund({ from: investor2, value: amount2 });
     (await instance.investors(0))[1].should.bignumber.equal(amount1);
@@ -56,10 +55,14 @@ contract('CrowdFunding', ([owner, investor1, investor2]) => {
   });
 
   it('should success the campaign if totalAmount is reached by deadline, then send balance to owner', async () => {
-    let amount1 = new web3.BigNumber(web3.toWei(1, 'ether'));
+    const amount1 = new web3.BigNumber(web3.toWei(1, 'ether'));
+    const pre = web3.eth.getBalance(owner);
     await instance.fund({ from: investor1, value: amount1 });
     await increaseTime(duration.hours(1));
-    await instance.checkGoalReached({ from: owner });
+    const res = await instance.checkGoalReached({ from: owner });
+    const gasCost = getTransactionGasCost(res['tx']);
+    const post = web3.eth.getBalance(owner);
+    post.minus(pre).plus(gasCost).should.be.bignumber.equal(amount1);
     (await instance.status()).should.be.equal("Campaign Succeeded");
     (await instance.ended()).should.be.true;
     (await web3.eth.getBalance(instance.address)).should.bignumber.equal(0);
@@ -76,8 +79,8 @@ contract('CrowdFunding', ([owner, investor1, investor2]) => {
     post.minus(pre).plus(gasCost).should.be.bignumber.equal(0);
     (await instance.status()).should.be.equal("Campaign Failed");
     (await instance.ended()).should.be.true;
+    (await web3.eth.getBalance(instance.address)).should.bignumber.equal(0);
   });
-
 
 });
 
